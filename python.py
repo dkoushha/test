502d80e520863390cf46ab024611c7f262417228
dkoushha <dimakoushha@gmail.com>
2020-07-31 19:42:58 +0200

    check if the commit working

--- python.py
+++ python.py
@@ -3,9 +3,9 @@ print(sys.version)
 print('something')
 
 
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = 'Hello, {}'.format(who_to_greet)
+#     return greeting
 
 
 # print(greet('World'))
