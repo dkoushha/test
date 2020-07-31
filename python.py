ffd1343925acff33a3469633d32e9ee581e7862f
dkoushha <dimakoushha@gmail.com>
2020-07-31 19:12:45 +0200

    first commit

---
+++ python.py
@@ -0,0 +1,13 @@
+import sys
+print(sys.version)
+print('something')
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+
+print(greet('World'))
+print(greet('Name'))
+print(greet('Name'))
